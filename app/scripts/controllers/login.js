e1093c7a7dc2d1d20b2123f7fcad03716899ea1d
Julian Poyourow <julianpoyo@gmail.com>
2015-04-27 13:39:29 -0700

    Setup login submission

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -14,5 +14,24 @@ angular.module('clientportalWebappApp')
       'AngularJS',
       'Karma'
     ];
+
     $scope.loginData = {};
+
+    $scope.doLogin = function(){
+        //Send login json, and store the response
+        $scope.serverResponse = Client.login(loginData, function(){
+            //Determine response from server
+            if (!$scope.serverResponse.result) {
+                alert("Sorry, that isn't the correct username and password.");
+            } else {
+                //Store the token from the server for future use
+                document.cookie = "session_token=" + $scope.serverResponse.result.session_token + "; expires=Sun, 18 Jan 2037 12:00:00 GMT";
+                //Redirect to main site
+                $location.path("/");
+            }
+        },
+        //To catch Errors when trying to join while the server
+        //Or backend is down
+        catchError);
+    }
   });
