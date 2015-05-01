928151eff4e8946b2b5124cbaaa3cb90a09e1a6c
Julian Poyourow <julianpoyo@gmail.com>
2015-05-01 14:44:40 -0700

    Updated panel page

--- app/scripts/controllers/panel.js
+++ app/scripts/controllers/panel.js
@@ -8,10 +8,20 @@
  * Controller of the clientportalWebappApp
  */
 angular.module('clientportalWebappApp')
-  .controller('PanelCtrl', function ($scope) {
+  .controller('PanelCtrl', function ($scope, $location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    //Fetch the session token for use
+    $scope.session_token = getCookie("session_token");
+    //Check if user is logged in. No: Goto login. Yes: Get user data
+    if(!$scope.session_token){
+        $location.path("/login");
+    } else {
+        Client.get($scope.session_token);
+    }
+
   });
